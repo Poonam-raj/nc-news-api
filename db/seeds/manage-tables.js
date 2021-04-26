9b26b03707e96323c3d4e0c61cf61b1ce7b7f582
Alison Miles <alison_miles@outlook.com>
2021-04-26 13:02:55 +0100

    working on seeding

--- db/seeds/manage-tables.js
+++ db/seeds/manage-tables.js
@@ -1,6 +1,6 @@
-const db = require("../connection.js");
+const db = require('../connection.js');
 
-const createTables = async () => {
+exports.createTables = async () => {
   await db.query(
     `CREATE TABLE topic (slug VARCHAR PRIMARY KEY, description VARCHAR);`
   );
@@ -13,4 +13,11 @@ const createTables = async () => {
   await db.query(
     `CREATE TABLE comment (comment_id SERIAL PRIMARY KEY, author VARCHAR REFERENCES user(username), article_id INT REFERENCES article(article_id), votes INT DEFAULT 0, created_at BIGINT, body VARCHAR);`
   );
+};
+
+exports.dropTables = async () => {
+  await db.query(`DROP TABLES IF EXISTS comment`);
+  await db.query('DROP TABLE IF EXISTS article');
+  await db.query('DROP TABLE IF EXISTS user');
+  await db.query('DROP TABLE IF EXISTS topic');
 };
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,5 +1,18 @@
-exports.seed = function () {
+const db = require('../connection');
+const { createTables, dropTables } = require('../seeds/manage-tables');
+const format = require('pg-format');
+
+
+const seed = async({articleData, topicData, userData, commentData}) => {
+  await dropTables();
+  await createTables();
+  const insertTopicsQueryString = format(`INSERT INTO topics (slug, description) VALUES %L RETURNING *;`, topicData.map(({description, slug}) => [description, slug]));
+  const insertingTopics = await db.query(insertTopicsQueryString);
+  console.log(insertingTopics);
+};
+
+seed();
+
   // add seeding functionality here
   // this function should take as argument(s) all the data it needs to seed
   // it should insert this data into the relevant tables in your database
-};
