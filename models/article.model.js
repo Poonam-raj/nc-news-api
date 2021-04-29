88f8bedf11290f12eec48d6ca444175989b53848
Poonam-raj <p_rajput@hotmail.co.uk>
2021-04-29 12:26:10 +0100

    modifying article PATCH model to simplify 'malformedBody' if statement and separate out utility function. Adding default sort and order into fetchAllArticles

--- models/article.model.js
+++ models/article.model.js
@@ -1,4 +1,5 @@
 const db = require("../db/connection");
+const { isMalformedBody } = require("./utils.models");
 
 exports.fetchArticle = async (articleID) => {
   const articleResponse = await db.query(
@@ -8,42 +9,36 @@ exports.fetchArticle = async (articleID) => {
     GROUP BY article.article_id;`,
     [articleID]
   );
-  const article = articleResponse.rows;
+  const { rows } = articleResponse;
 
-  if (article.length === 0) {
+  if (articleResponse.rowCount === 0) {
     return Promise.reject({
       status: 404,
-      msg: `No article found for article_id: ${articleID}.`,
+      msg: `Article_id: ${articleID} is invalid.`,
     });
   }
-  return article;
+
+  return rows;
 };
 
 exports.updateArticle = async (articleID, body) => {
   const { inc_votes } = body;
-  const bodyKeys = Object.keys(body);
-
-  if (!inc_votes || typeof inc_votes !== "number" || bodyKeys.length > 1) {
-    return Promise.reject({
-      status: 400,
-      msg: `Bad request: malformed body`,
-    });
-  } else {
-    await db.query(
-      `UPDATE article SET 
+  await isMalformedBody(inc_votes, body);
+  await db.query(
+    `UPDATE article SET 
   votes = votes + $1 WHERE article_id = $2;`,
-      [inc_votes, articleID]
-    );
-    return this.fetchArticle(articleID);
-  }
+    [inc_votes, articleID]
+  );
+  return this.fetchArticle(articleID);
 };
 
-exports.fetchAllArticles = async () => {
+exports.fetchAllArticles = async (sort_by = "created_at", order = "DESC") => {
   const articlesResponse = await db.query(
     `SELECT article.author, title, article.article_id, topic, article.created_at, article.votes, COUNT(comment.comment_id) AS comment_count FROM article 
     LEFT JOIN comment ON comment.article_id = article.article_id
-    GROUP BY article.article_id;`
+    GROUP BY article.article_id
+    ORDER BY ${sort_by} ${order};`
   );
-  const articles = articlesResponse.rows;
-  return articles;
+  const { rows } = articlesResponse;
+  return rows;
 };
