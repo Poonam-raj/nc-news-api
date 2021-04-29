d08f0202784aaf2901ed6720c5e60ac2d7686f48
Poonam-raj <p_rajput@hotmail.co.uk>
2021-04-29 16:38:00 +0100

    removing utility that was unused to validate sort_by columns

--- models/utils.models.js
+++ models/utils.models.js
@@ -1,5 +1,3 @@
-const db = require("../db/connection");
-
 exports.isMalformedBody = (key, body) => {
   const bodyKeys = Object.keys(body);
   if (typeof key !== "number" || bodyKeys.length > 1) {
@@ -9,12 +7,3 @@ exports.isMalformedBody = (key, body) => {
     });
   }
 };
-
-// exports.checkColumnExists = async (sort_by) => {
-//   const result = await db.query(`SELECT $1 FROM article;`, [sort_by]);
-//   console.log(result);
-//   if (result.rowCount === 0) {
-//     return false;
-//   }
-//   return true;
-// };
