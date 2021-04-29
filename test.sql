44752200446857409a0a7f74053d12b954d1881c
Poonam-raj <p_rajput@hotmail.co.uk>
2021-04-29 15:40:47 +0100

    amended gitignore to include test sql and test txt files

--- test.sql
+++ test.sql
@@ -1,5 +1,3 @@
 \c nc_news_test;
-SELECT topic.*, COUNT(article.article_id) AS article_count FROM topic 
-    LEFT JOIN article ON article.topic = topic.slug
-    GROUP BY topic.slug;
+SELECT dog FROM article;
 
