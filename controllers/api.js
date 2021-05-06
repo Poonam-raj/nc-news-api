230fbb957e0724748ad7bb6ae9062e3d4c78449f
Poonam-raj <p_rajput@hotmail.co.uk>
2021-05-06 10:27:46 +0100

    adding controller for GET /api endpoint, passing tests

--- controllers/api.js
+++ controllers/api.js
@@ -1,9 +1,17 @@
-const { selectTopics } = require("../models/api");
+const { selectTopics, fetchEndpoints } = require("../models/api");
 
 exports.getTopics = (req, res, next) => {
   selectTopics()
     .then((topics) => {
       res.status(200).send({ topics });
+    })
+    .catch(next);
+};
+
+exports.getEndpoints = (req, res, next) => {
+  fetchEndpoints()
+    .then((endpoints) => {
+      res.status(200).send({ endpoints });
     })
     .catch(next);
 };
