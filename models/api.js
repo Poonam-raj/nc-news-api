9408f4f1f861efe254cb50560ee9856c463aee08
Poonam-raj <p_rajput@hotmail.co.uk>
2021-05-06 10:27:09 +0100

    adding passing code to read endpoints.JSON and return it to the controller

--- models/api.js
+++ models/api.js
@@ -1,6 +1,13 @@
 const db = require("../db/connection");
+const fs = require("fs/promises");
 
 exports.selectTopics = async () => {
   const topicResponse = await db.query(`SELECT * FROM topics;`);
   return topicResponse.rows;
+};
+
+exports.fetchEndpoints = async () => {
+  const readingEndpoints = await fs.readFile("./endpoints.json", "utf-8");
+  const parsedEndpoints = JSON.parse(readingEndpoints);
+  return parsedEndpoints;
 };
