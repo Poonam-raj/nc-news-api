0615106857d3a58e2c28eb7837d0d0bda6465b9f
Poonam-raj <p_rajput@hotmail.co.uk>
2021-05-06 10:29:23 +0100

    adding GET /api route

--- routers/api.js
+++ routers/api.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { getTopics } = require("../controllers/api");
+const { getTopics, getEndpoints } = require("../controllers/api");
 const articleRouter = require("./article");
 
 const apiRouter = express.Router();
 
+apiRouter.get("/", getEndpoints);
 apiRouter.get("/topics", getTopics);
 apiRouter.use("/articles", articleRouter);
 
