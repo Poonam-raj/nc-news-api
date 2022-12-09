4de67acb1a8a7a4075860c7ec437d8d7b803e081
Poonam <p_rajput@hotmail.co.uk>
2022-12-09 13:06:41 +0000

    more routing for users and comments

--- routers/api.js
+++ routers/api.js
@@ -1,11 +1,16 @@
-const express = require("express");
-const { getTopics, getEndpoints } = require("../controllers/api");
-const articleRouter = require("./article");
+const express = require('express');
+const { getTopics, getEndpoints } = require('../controllers/api');
+
+const articleRouter = require('./article');
+const commentsRouter = require('./comments');
+const userRouter = require('./user');
 
 const apiRouter = express.Router();
 
-apiRouter.get("/topics", getTopics);
-apiRouter.use("/articles", articleRouter);
-apiRouter.get("/", getEndpoints);
+apiRouter.get('/topics', getTopics);
+apiRouter.use('/users', userRouter);
+apiRouter.use('/articles', articleRouter);
+apiRouter.use('/comments', commentsRouter);
+apiRouter.get('/', getEndpoints);
 
 module.exports = apiRouter;
