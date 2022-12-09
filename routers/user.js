1a582892ff3a7239479f3eaa8f9bb5627b1f9182
Poonam <p_rajput@hotmail.co.uk>
2022-12-09 13:05:51 +0000

    GET users

---
+++ routers/user.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const { getUsers } = require('../controllers/users');
+const userRouter = express.Router();
+userRouter.get('/', getUsers);
+
+module.exports = userRouter;
